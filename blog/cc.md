a3e351e0cfae217f7b989d9b6ff3c5475ab99a64
Nguyen Quang Vinh <63202399+vinhveer@users.noreply.github.com>
2025-06-14 13:42:26 +0700

    Hello

---
+++ blog/cc.md
@@ -0,0 +1,5 @@
+---
+title: Cài đặt Microsoft Office 365
+slug: install-office-365
+date: 2025-01-12
+---
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,12 +1,4 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -14,24 +6,16 @@ const config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://vinhveer.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'vinhveer', // Usually your GitHub org/user name.
-  projectName: 'vinhveer.github.io', // Usually your repo name.
+  organizationName: 'vinhveer',
+  projectName: 'vinhveer.github.io',
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -42,14 +26,14 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: false, // Tắt tài liệu nếu không cần
+        docs: false,
         blog: {
-          showReadingTime: true, // Hiển thị thời gian đọc bài viết
-          path: 'blog', // Đường dẫn thư mục chứa blog
-          routeBasePath: 'blog', // URL cơ sở cho blog (ví dụ: /blog)
-          blogSidebarCount: 'ALL', // Hiển thị tất cả bài viết trên sidebar
-          blogSidebarTitle: 'All posts', // Tiêu đề sidebar
-          postsPerPage: 5, // Số bài viết mỗi trang
+          showReadingTime: true,
+          path: 'blog',
+          routeBasePath: 'blog',
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'All posts',
+          postsPerPage: 5,
           include: ['*.md', '*.mdx'],
         },
         theme: {
@@ -62,7 +46,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Vinh Veer',
@@ -71,7 +54,6 @@ const config = {
           src: 'img/logo.png',
         },
         items: [
-          { to: '/about-me', label: 'Về tôi', position: 'left' },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'http://ec2-50-17-60-120.compute-1.amazonaws.com/',
@@ -89,46 +71,20 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'GitHub',
+                href: 'https://github.com/vinhveer',
               },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
               {
                 label: 'Blog',
                 to: '/blog',
               },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Vinh Veer.`,
       },
       prism: {
         theme: prismThemes.github,
